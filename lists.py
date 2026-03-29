bf5db8c92623322a9c9bb9da05bc4e484c408ab8
Mete <backendDevEn@protonmail.com>
2026-03-29 17:44:22 +0300

    Exercises on page 36 completed

---
+++ lists.py
@@ -0,0 +1,14 @@
+# Working with Lists
+car_brands = ["Tesla","Honda","Toyota", "Bugatti", "hyindai","Togg","Seat"]
+print(car_brands)
+print(f"{car_brands[0]} is my favorite car brand!")
+print(f"{car_brands[4].capitalize()} is trash")
+print(f"I'd like to own {car_brands[3]}")
+
+# Exercises 36
+names = ["Michael","Mitchel","Grace","Clara","Mete"]
+print(f"How are you doing{names[0]}?")
+print(f"How are you doing{names[1]}?")
+print(f"How are you doing{names[2]}?")
+print(f"How are you doing{names[3]}?")
+print(f"How are you doing{names[4]}?")
