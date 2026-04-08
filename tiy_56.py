89d0f28b0ec36da10f78f7b78318441cad7541e0
Mete <backendDevEn@protonmail.com>
2026-04-08 12:17:45 +0300

    TIY on page 56 completed

---
+++ tiy_56.py
@@ -0,0 +1,13 @@
+# Pizzas (4-1)
+pizzas = ['pepperoni','margarita','african']
+for pizza in pizzas:
+    print(f"{pizza} is one of my favorite kind of pizza. \n")
+
+print('I like pizzas very much!')
+
+# Animals (4-2)
+animals = ['panther','lion','tiger','cat']
+for animal in animals:
+    print(f"{animal.title()} is a great animal!")
+
+print("All these animals are from the cat family (felines)")
