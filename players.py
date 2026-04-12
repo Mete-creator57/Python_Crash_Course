42eb08d55d9eda4a34974c2fc3b8048891840440
Mete <backendDevEn@protonmail.com>
2026-04-12 15:46:23 +0300

    Copying list using a slice topic covered

---
+++ players.py
@@ -0,0 +1,12 @@
+
+players = ['James','Thomas','Betsy','Mary','John']
+print(players[2:])
+
+# Looping Through a Slice
+for player in players[:2]:
+    print(player.title())
+
+# Copying a List using slice
+best_players = players[1:4]
+print(best_players)
+print(players)
