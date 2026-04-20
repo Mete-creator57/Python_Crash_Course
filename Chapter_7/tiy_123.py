483cba68a4aeca7207c277ab35496dd514d1fbe8
Mete <backendDevEn@protonmail.com>
2026-04-20 12:46:21 +0300

    Completed TIY on oage 123

---
+++ Chapter_7/tiy_123.py
@@ -0,0 +1,38 @@
+
+# Pizza Toppings (7-4)
+pizza = []
+while True:
+    topping = input('Enter a topping: ')
+    if topping == 'quit':
+        break
+    else:
+         print(f'Adding {topping} to your pizza!')
+         pizza.append(topping)
+    
+print(pizza)
+
+fee = 0
+# Movie Tickets (7-5)
+while True:
+    age = input('Enter your age: ')
+    if age == 'q':
+        break
+    age = int(age)
+
+    if age < 3 and age > 0:
+        print('The ticket is free!')
+        break
+
+    elif age >= 3 and age <= 12:
+        fee = 10
+        print(f'The ticket is {fee}$')
+        break
+
+    elif age > 12:
+        fee = 15
+        print(f'The ticket is {fee}$')
+        break
+
+    else:
+        print('Invalid age')
+        continue
