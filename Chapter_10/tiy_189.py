b7b2640c429e73697e859e470c2495b963597c38
Mete <backendDevEn@protonmail.com>
2026-06-21 14:53:46 +0300

    refactor: Explain replace() method behaviour

--- Chapter_10/tiy_189.py
+++ Chapter_10/tiy_189.py
@@ -1,10 +1,16 @@
 # Learning Python (10-1) -- skipped
 
 # Leraning C (10-2)
-text = 'My name is Mete'
+text = "My friend's name is Mete and Mete is a very smart boy"
 # replacing a word in the text
-text = text.replace('Mete', 'unknown', 9)
-print(text)
+# 1 = an optional argument indicating how many times 
+# 'Mete' will be replaced by the word 'unknown', in this case
+# only once
+text = text.replace('Mete', 'unknown', 1)
+
+# prints...
+# My friend's name is unknown and Mete is a very smart boy
+print(text) 
 
 # Simpler Code (10-3)
 # Done
