968cf147c065aea57c41f0786e6737aa9c06bb1d
Mete <backendDevEn@protonmail.com>
2026-07-21 13:44:28 +0300

    feat: add the bullet settings

--- Part_2_Projects/settings.py
+++ Part_2_Projects/settings.py
@@ -10,3 +10,9 @@ class Settings:
         # ship settings
         self.ship_speed = 10
         self.rocket_speed = 20
+
+        # Bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
